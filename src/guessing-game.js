43e5a58660c7115f0ae44da65b36ca3bc7015630
MatusVit <matvi@mail.ru>
2019-10-10 17:37:45 +0300

    feat: not correct version

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,27 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+
+    }
+
+      
 
     setRange(min, max) {
+        this.min = min;
+        this.max = max;
 
     }
 
     guess() {
-
+        this.number =  Math.floor(this.min + Math.random()*(this.max-this.min));
+        return this.number;
     }
 
     lower() {
-
+        this.max = this.number;
     }
 
     greater() {
-
+        this.min = this.number;
     }
 }
 
